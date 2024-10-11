9dae3a44ae858d45f0f1bbd770219f03b55b4333
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2024-10-11 11:41:14 -0300

    note item component

---
+++ src/components/notes/NoteItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { note } = defineProps({
+  note: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="card h-100">
+    <div class="card-header">{{ note.title }}</div>
+    <div class="card-body">
+      <p class="card-text">
+        {{ note.content }}
+      </p>
+    </div>
+  </div>
+</template>
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+import NoteItem from '@/components/notes/NoteItem.vue'
+</script>
 
 <template>
   <h1>Notas view</h1>
+  <NoteItem
+    :note="{
+      title: 'nota de prueba',
+      content: 'contenido de la nota de prueba',
+    }"
+  />
 </template>
